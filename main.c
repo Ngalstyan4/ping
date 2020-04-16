13bb1204ce011118d3d848c6726a2a44a938e64f
Narek Galstyan <narekg@princeton.edu>
2020-04-15 22:47:01 -0700

    More cosmetics + factor out socket initialization

--- main.c
+++ main.c
@@ -25,8 +25,9 @@ typedef struct RTT_time {
     int cnt;
 } RTT_time;
 
-/* These need to be global so the signal handler can access
- */
+/***************************** DANGER ZONE BEGIN ********************************/
+/* These need to be global so the SIGINT signal handler can access them*/
+
 char *host;
 int ntransmitted = 0, nreceived = 0;
 /* sine we timeout at 1s, 2020ms is more than enough for init min value.*/
@@ -34,19 +35,22 @@ RTT_time rtt_time_stats = {.min=2020., .max=0.0, .sum=0.0, .sumsq=0.0, .cnt=0};
 
 u_short ICMP_ID;
 
+/***************************** DANGER ZONE END ********************************/
+
+int init_socket(struct timeval *recv_timeout, struct sockaddr *whereto, socklen_t *whereto_len);
 
 int compose_packet(char *packet, u_short seq_num, struct timeval *send_time);
 
 float update_stats(struct RTT_time *rtt_time_stats, struct timeval *recv_time, struct timeval *send_time_in_data);
 
 void sigint_print_stats(int sig_num);
+
 u_short icmp_checksum(char *packet, int len);
 
 void print_usage();
 
 int main(int argc, char **argv) {
     char packet[64];
-    struct addrinfo hints, *servinfo, *p;
 
     // for sending packets
     struct sockaddr whereto;
@@ -56,85 +60,55 @@ int main(int argc, char **argv) {
     struct sockaddr wherefrom;
     socklen_t wherefrom_len = sizeof(wherefrom);
 
+    // general var for functions that return int/status
     int status;
-    int ret, sockfd;
-    struct icmp *icmp;
+    int sockfd;
 
     // seconds to wait after each request
-    unsigned int wait = 1;
+    unsigned int interrequest_wait = 1;
 
     // timeout in (seconds, useconds)
     // initial value relevant only for the first packet
     // then it is updated to 2*RTT //todo
     struct timeval recv_timeout = {1, 0};
-
     int seq_num = 0;
+
     if (argc < 2 || argc > 2) {
         print_usage();
         exit(1);
     }
     ICMP_ID = getpid() & 0xffff;
     host = argv[1];
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // use AF_INET6 to force IPv6
-    hints.ai_socktype = SOCK_RAW;
 
-    // gethostbyname is simpler but does not work well with IPv6
-    if ((ret = getaddrinfo(host, NULL, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "client: getaddrinfo: %s\n", gai_strerror(ret));
-        return 1;
-    }
-
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-//    printf("socket, ai soctype %d %d", SOCK_RAW, hints.ai_socktype);
-        if ((sockfd = socket(p->ai_family, SOCK_RAW, IPPROTO_ICMP)) == -1) {
-            perror("client: socket\n");
-            continue;
-        }
-
-        // set timeout for recvfrom
-        status = setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &recv_timeout, sizeof recv_timeout);
-        if (status != 0) {
-            perror("client: socket: set recv timeout\n");
-            continue;
-        }
-        // copy to be able to free servinfo
-        whereto = *p->ai_addr;
-        whereto_len = p->ai_addrlen;
-        break;
-    }
-    freeaddrinfo(servinfo);
-
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        exit(1);
-    }
+    sockfd = init_socket(&recv_timeout, &whereto, &whereto_len);
 
     printf("myPING %s (%s):\n", host, inet_ntoa(((struct sockaddr_in *) &whereto)->sin_addr));
-    seq_num = 0;
     signal(SIGINT, sigint_print_stats);
+
+    seq_num = 0;
     for (;;) {
         struct icmp *icmp_header;
         struct ip *ip_header;
         struct timeval send_time, recv_time;
         int size;
 
+        gettimeofday(&send_time, NULL);
+
         // seq num can overflow but it is not that critical and we can hope it just wraps around
         // could do modulo arithmetic but thought it would be an overkill
-        gettimeofday(&send_time, NULL);
         size = compose_packet(packet, seq_num++, &send_time);
-        status = sendto(sockfd, packet, size, 0, &whereto, whereto_len);//todo fix hardcoded 16
+        status = sendto(sockfd, packet, size, 0, &whereto, whereto_len);
         if (status == -1) {
-            printf("sendto: unable to send to %s, icmp_seq=%u never sent\n",
+            printf("myping: sendto: unable to send to %s, icmp_seq=%u never sent\n",
                    inet_ntoa(((struct sockaddr_in *) &whereto)->sin_addr), seq_num - 1);
-            sleep(wait);
-            /* looks like regular ping does not continue here and tried to receive the packet with icmp_seq
+            sleep(interrequest_wait);
+            /* looks like regular ping does not continue here and tries to receive the packet with icmp_seq
              * even if it never got sent. I think there is no point of that and so will continue here.
              */
             continue;
         }
-        ntransmitted++;
 
+        ntransmitted++;
         /* ignore received packets if we have already announced them as lost */
         do {
             status = recvfrom(sockfd, packet, sizeof(packet), 0, &wherefrom, &wherefrom_len);
@@ -151,16 +125,70 @@ int main(int argc, char **argv) {
 
             nreceived++;
             rtt = update_stats(&rtt_time_stats, &recv_time, send_time_in_data);
+            if (rtt_time_stats.cnt == 3) {
+                /* improvising here:
+                 * Wait some time to accumulate data then update recv_time
+                 * */
+
+                memset(&recv_timeout, 0, sizeof(recv_timeout));
+                // TIMEOUT = 2*RTT
+                recv_timeout.tv_usec = 2 * rtt_time_stats.sum * 1000.0 / rtt_time_stats.cnt;
+                setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &recv_timeout, sizeof(recv_timeout));
+            }
+
             printf("received icmp packet of %d bytes from %s, icmp_seq=%u time=%.3fms \n", status,
                    inet_ntoa(((struct sockaddr_in *) &wherefrom)->sin_addr), icmp_header->icmp_seq, rtt);
         } else {
             fprintf(stderr, "Request timeout for icmp_seq %u\n", seq_num - 1);
         }
-        sleep(wait);
+        sleep(interrequest_wait);
 
     }
 
 
+}
+
+/* Initiates a raw socket and returns a valid socket or exits the program with an error message */
+int init_socket(struct timeval *recv_timeout, struct sockaddr *whereto, socklen_t *whereto_len) {
+    int sockfd;
+    int status;
+    struct addrinfo hints, *servinfo, *p;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; // use AF_INET6 to force IPv6
+    hints.ai_socktype = SOCK_RAW;
+
+    // gethostbyname is simpler but does not work well with IPv6
+    if ((status = getaddrinfo(host, NULL, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "myping: getaddrinfo: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        //printf("socket, ai soctype %d %d", SOCK_RAW, hints.ai_socktype);
+        if ((sockfd = socket(p->ai_family, SOCK_RAW, IPPROTO_ICMP)) == -1) {
+            perror("myping: socket\n");
+            continue;
+        }
+
+        // set timeout for recvfrom
+        status = setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *) recv_timeout, sizeof(struct timeval));
+        if (status != 0) {
+            perror("myping: socket: set recv timeout\n");
+            continue;
+        }
+        // copy to be able to free servinfo
+        *whereto = *p->ai_addr;
+        *whereto_len = p->ai_addrlen;
+        break;
+    }
+    freeaddrinfo(servinfo);
+
+    if (p == NULL) {
+        fprintf(stderr, "myping: failed to connect\n");
+        exit(1);
+    }
+    return sockfd;
 }
 
 /* Compose ICMP message in packet and return its size (includes header and data) */
@@ -201,11 +229,14 @@ float update_stats(struct RTT_time *rtt_time_stats, struct timeval *recv_time, s
 }
 
 void sigint_print_stats(int sig_num) {
-    printf("---%s myping statistics ---\n", host);
-    printf("%d packets transmitted, %d packets received, %.1f%% packet loss\n", ntransmitted, nreceived, (ntransmitted-nreceived) * 100.0/ntransmitted);
-    printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms", rtt_time_stats.min, rtt_time_stats.sum/rtt_time_stats.cnt,
-            rtt_time_stats.max,
-           sqrt((rtt_time_stats.sumsq-rtt_time_stats.sum)/rtt_time_stats.cnt));
+    /* Note: no need to reset the signal since we are exiting the program */
+    printf("--- %s myping statistics ---\n", host);
+    printf("%d packets transmitted, %d packets received, %.1f%% packet loss\n", ntransmitted, nreceived,
+           (ntransmitted - nreceived) * 100.0 / ntransmitted);
+    printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms", rtt_time_stats.min,
+           rtt_time_stats.sum / rtt_time_stats.cnt,
+           rtt_time_stats.max,
+           sqrt((rtt_time_stats.sumsq - rtt_time_stats.sum) / rtt_time_stats.cnt));
     exit(0);
 }
 
